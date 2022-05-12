96621c2db243c55d35cc635c95a5401b358d19fc
Wolfy4H <Leon.Kersten@outlook.com>
2022-05-12 11:28:37 +0200

    geholpen, begin gemaakt aan schema

--- Z-Basics/schema.php
+++ Z-Basics/schema.php
@@ -6,5 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
   </head>
-  <body></body>
+  <body>
+    <div class="Space">
+      <div class="left-top"></div>
+      <div class="right-top"></div>
+      <div class="left-bot"></div>
+      <div class="right-bot"></div>
+    </div>
+  </body>
 </html>
+
+<!---- kleur: F1F1F1 (white), 23A99B (green-blue), 5DC6EC(lichtblauw), FF6600 (orange), FFFF28 (geel)----->
