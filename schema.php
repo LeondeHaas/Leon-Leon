4bd4f5ef36dd2363b78d4a30cfb23b438f47ad3a
LeondeHaas <leonthedragonslayer123@gmail.com>
2022-05-30 12:59:32 +0200

    schema is klaar voor gebruik

--- schema.php
+++ schema.php
@@ -1,22 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <link rel="stylesheet" href="styles.css" />
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap"
+      rel="stylesheet"
+    />
     <title>Document</title>
   </head>
+  <!-- <header>
+    <div class="header">
+      <div class="logo">
+        <img class="img" src="igv/img/islandlogo.png" alt="" />
+      </div>
+      <div class="pages">
+        <a class="pg" href="/index.php"><h3>Home</h3></a>
+        <a class="pg" href=/boeken.php"><h3>Boeken</h3></a>
+        <a class="pg" href="/schema.php"><h3>Schema</h3></a>
+        <a class="pg" href="/contact.php"><h3>Helpdesk</h3></a>
+        <a class="pg" href="/inloggen.php"><h3>Inloggen</h3></a>
+        <img class="favs" src="igv/img/star.png" alt="" />
+      </div>
+    </div>
+  </header> -->
   <body>
-    <?php
-      include("../includes/header.php");
-    ?>
-    <main>
-        
-    </main>
-    <footer>
-
-    </footer>
+    <div class="containerschema">
+      <div class="schema">
+        <div class="schemainfo"><h3>Vertrek: 15:35</h3></div>
+        <div class="schemainfo"><h3>Aankomst: 20:40</h3></div>
+        <div class="schemainfo"><h3>Bestemming: Spanje</h3></div>
+        <div class="schemainfo"><h3>Gate: A56</h3></div>
+        <div class="schemainfo"><h3>Snacks: XXL Del</h3></div>
+      </div>
+    </div>
   </body>
 </html>
-
-<!---- kleur: F1F1F1 (white), 23A99B (green-blue), 5DC6EC(lichtblauw), FF6600 (orange), FFFF28 (geel)----->
