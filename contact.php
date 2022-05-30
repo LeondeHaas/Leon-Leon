d02cc8de270e613a4f46e9937f3dc5f6e69d3fb6
Wolfy4H <Leon.Kersten@outlook.com>
2022-05-30 12:59:40 +0200

    header bij gewerkt

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" href="styles.css"/>
     <link
       href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@1,300&family=Macondo&display=swap"
       rel="stylesheet"
@@ -18,14 +18,18 @@
       <!----moving background--->
       <div class="backgroundborder">
         <video autoplay muted loop> 
-          <source src="igv/video/tro.mp4" type="video/mp4">
+          <source src="igv/video/tropical-island2.mp4" type="video/mp4">
         </video>
     <div class="background"></div>
 
     <?php
     include("includes/footer.php")
     ?>
+    <footer>
+        <p>  © Leon's | LionFlights | 🦁💢 </p>
+    </footer>    
   </body>
 </html>
 
 <!---- kleur: F1F1F1 (white), 23A99B, 5DC6EC, FF6600, FFFF28 ----->
+
--- includes/header.php
+++ includes/header.php
@@ -5,10 +5,10 @@
       </div>
       <div class="pages">
         <a class="pg" href="/index.php"><h3>Home</h3></a>
-        <a class="pg" href="boeken.php"><h3>Boeken</h3></a>
-        <a class="pg" href="schema.php"><h3>Schema</h3></a>
-        <a class="pg" href="contact.php"><h3>Helpdesk</h3></a>
-        <a class="pg" href="inloggen.php"><h3>Inloggen</h3></a>
+        <a class="pg" href="/boeken.php"><h3>Boeken</h3></a>
+        <a class="pg" href="/schema.php"><h3>Schema</h3></a>
+        <a class="pg" href="/contact.php"><h3>Helpdesk</h3></a>
+        <a class="pg" href="/inloggen.php"><h3>Inloggen</h3></a>
         <img class="favs" src="igv/img/star.png" alt="" />
       </div>
     </div>
