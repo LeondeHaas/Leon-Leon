e626b917f8627a7b923cb7e165ec450d45489b88
LeondeHaas <leonthedragonslayer123@gmail.com>
2022-05-30 13:28:02 +0200

    boeken is klaar voor gebruik

--- boeken.php
+++ boeken.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <link rel="stylesheet" href="styles.css" />
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-</head>
-<body>
-    
-</body>
+  </head>
+  <body>
+    <div class="containerboeken">
+      <div class="bestemming">
+        <img class="img" src="/images/pexels-ingo-joseph-188029.jpg" alt="" />
+        <div class="boekeninfo"></div>
+        <div class="boekeninfo"></div>
+        <div class="boekeninfo"></div>
+      </div>
+    </div>
+  </body>
 </html>
