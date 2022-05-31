c0819220de6b89f53175a4bfc4a941f05cde37bc
LeondeHaas <leonthedragonslayer123@gmail.com>
2022-05-31 12:03:28 +0200

    header is responsive

--- schema.php
+++ schema.php
@@ -10,17 +10,38 @@
       href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@1,300&family=Macondo&display=swap"
       rel="stylesheet"
     />
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-</head>
+  </head>
+  <?php
+    include("includes/header.php")
+    ?>
   <body>
-    <div class="containerschema">
-      <div class="schema">
-        <div class="schemainfo"><h3>Vertrek: 15:35</h3></div>
-        <div class="schemainfo"><h3>Aankomst: 20:40</h3></div>
-        <div class="schemainfo"><h3>Bestemming: Spanje</h3></div>
-        <div class="schemainfo"><h3>Gate: A56</h3></div>
-        <div class="schemainfo"><h3>Snacks: XXL Del</h3></div>
+    <div class="backgroundborder">
+      <video autoplay muted loop>
+        <source src="igv/video/production ID_3678380.mp4" type="video/mp4" />
+      </video>
+      <div class="hidden"></div>
+      <div class="containerschema">
+          <table class="schematext">
+    <tr>
+      <th><h2>Vertrek<h2></th>
+      <th><h2>Aankomst<h2></th>
+      <th><h2>Gate<h2></th>
+       <th><h2>Bestemming<h2></th>
+       <th><h2>Vliegtuig<h2></th>
+    </tr>
+    <tr>
+      <td>18:35</td>
+      <td>20:45</td>
+      <td>A65</td>
+      <td>Amerika</td>
+      <td>De bazaar</td>
+    </tr>
+  </table>
+  
+</div>
       </div>
     </div>
   </body>
