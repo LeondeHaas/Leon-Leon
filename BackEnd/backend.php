abcfec98b6372e2f43313e670a896b9a37151ef7
LeondeHaas <leonthedragonslayer123@gmail.com>
2022-06-07 12:33:50 +0200

    backend heeft css

--- BackEnd/backend.php
+++ BackEnd/backend.php
@@ -1,13 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="css/backend.css" />
+    <link rel="stylesheet" href="../css/backend.css" />
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<header>
+    <div class="header">
+        <h3>Welkom terug Admin,</h3>
+        <a class="pg" href="../inloggen.php"><h3 class="logout">Uitloggen</h3></a>
+    </div>
+  </header>
 <body>
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum, consequuntur ut? Error similique, aspernatur perferendis ut vero cum, ipsa placeat itaque consectetur delectus neque molestias recusandae beatae voluptatum dolorum quisquam.
+    <div class="hidden"></div>
+    <div class="crud">
+        <button class="create"><h1>Create</h1></button>
+         <button class="update"><h1>Update</h1></button>
+          <button class="delete"><h1>Delete</h1></button>
+    </div>
+   <div class="container">
+       <div class="containerinfo"></div>
+   </div>
+   <div class="container"></div>
+   <div class="container"></div>
 </body>
 </html>
