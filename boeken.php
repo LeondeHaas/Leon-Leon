5bb3a434d365e11045a551715571440b7daff329
Wolfy4H <Leon.Kersten@outlook.com>
2022-06-23 11:07:46 +0200

    alle javascript af

--- boeken.php
+++ boeken.php
@@ -126,6 +126,28 @@
         </div>
       </div>
       <!-- dit is een container boeken       -->
+      <button onclick="topFunction()" class="back_to_top" type="button"><h1>&#8593;</h1></button>
+      <script>
+        //Get the button:
+        mybutton = document.getElementById("back_to_top");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+          if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+          } else {
+            mybutton.style.display = "none";
+          }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+          document.body.scrollTop = 0; // For Safari
+          document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+      </script>
     </div>
      <?php
     include("includes/footer.php")
